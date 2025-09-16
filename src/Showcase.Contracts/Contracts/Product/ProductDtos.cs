4625efa7adaf11563c5e68f07429678375375422
J.T. Pierce <avacoder@hotmail.com>
2025-09-16 06:15:37 -0400

    Updated ProductDtos.cs

--- src/Showcase.Contracts/Contracts/Product/ProductDtos.cs
+++ src/Showcase.Contracts/Contracts/Product/ProductDtos.cs
@@ -1,19 +1,27 @@
 ﻿namespace Showcase.Contracts.Contracts.Product
 {
-    public class ProductCreateDto() {
+    public class ProductCreateDto()
+    {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; } = 0.0m;
     }
 
-    public record ProductUpdateDto(string Name, string Description, decimal Price);
+    public class ProductUpdateDto()
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+
     public record ProductReadDto(int Id, string Name, string Description, decimal Price, DateTime CreatedAt);
 
-    public class ProductDto() {
+    public class ProductDto()
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
-            }
+    }
 
 }
