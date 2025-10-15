c8cd306ce3b2cb09e40b8edc2e16c792fe1af361
J.T. Pierce <avacoder@hotmail.com>
2025-10-15 07:46:11 -0400

    Updated login page to handle errors and focus correctly

---
+++ src/Showcase.Client/wwwroot/js/focusHelper.js
@@ -0,0 +1,11 @@
+﻿window.focusHelper = {
+    focusById: function (id) {
+        const el = document.getElementById(id);
+        if (el) el.focus();
+    },
+
+    focusFirstInvalidInput: function () {
+        const invalid = document.querySelector(".input-validation-error");
+        if (invalid) invalid.focus();
+    }
+};
