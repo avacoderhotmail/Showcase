a0d3276a09520fc11360fc26d5d480cf8eded2a4
J.T. Pierce <avacoder@hotmail.com>
2025-10-15 09:33:25 -0400

    Fixed password validation on new user modal in Users.razor

--- src/Showcase.Contracts/Contracts/User/UserDTOs.cs
+++ src/Showcase.Contracts/Contracts/User/UserDTOs.cs
@@ -17,13 +17,19 @@ namespace Showcase.Contracts.Contracts.User
 {
     public class CreateUserDto
     {
+        [Required]
         public string Email { get; set; } = string.Empty;
 
         [DataType(DataType.Password)]
+        [Required]
         public string Password { get; set; } = string.Empty;
 
+        [Required]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required]
         public string DisplayName { get; set; } = string.Empty;
         public IEnumerable<string> Roles { get; set; } = Array.Empty<string>();
     }
